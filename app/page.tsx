5a8fe310d77516d6fef6cc2d8e6f3bd814ceac02
abhay kumar <abhay.k@digitalumbrel.la>
2023-12-07 15:40:08 +0530

    41th

--- app/page.tsx
+++ app/page.tsx
@@ -22,12 +22,12 @@ return (
 
       
         <div className={`${showUI?``:"hidden"} md:border-r-[1px] shadow-2xl md:shadow-[0px] `}>
-          <div className="border-b-[1px] flex justify-center items-center py-10">
-            <img src="/image/ooulet.png" alt="" className="w-1/2" />
+          <div className="border-b-[1px] hidden md:flex justify-center items-center py-10">
+            <img src="/image/ooulet.png" alt="" className="w-1/2 " />
           </div>
           <div className="px-8 py-8 flex flex-col gap-2">
             <img src="/image/1.jpg" alt="" className="w-[65px] h-[65px] rounded-full"/>
-            <p className="text-base font-semibold text-[#a2a2a2]">Ooulet - Create Website for Free</p>
+            <p className="text-base font-semibold text-[#a2a2a2] pt-4 md:pt-0">Ooulet - Create Website for Free</p>
             <h2 className="text-2xl font-semibold ">Free Phone Consultation</h2>
             <div className="flex flex-col gap-4">
               <div className="flex gap-4">
@@ -46,7 +46,7 @@ return (
             </div>
             <div className="md:hidden flex justify-center items-center">
             
-            <button onClick={handleScroll} className=" w-4/5 py-2 rounded-lg font-semibold text-white bg-[#EC0B43]">Schedule</button>
+            <button onClick={handleScroll} className="w-4/5 py-2 rounded-lg font-semibold text-white text-center bg-[#EC0B43]">Schedule</button>
       
           </div>
           </div>
